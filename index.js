72e145692474ca01f13a6548f027fec2e09bc3a2
viko16 <16viko@gmail.com>
2018-02-05 16:21:16 +0800

    feat: api design

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Talk is cheap. Show me the money!
+#!/usr/bin/env node
+
+const sade = require('sade')
+
+const prog = sade('repo')
+const pkg = require('./package.json')
+
+prog
+  .version(pkg.version)
+
+prog
+  .command('init')
+  .describe('Initial config files. (~/.tidy-repo/config.js)')
+  .action(() => {
+    console.log('run init')
+  })
+
+prog
+  .command('add <url>')
+  .describe('Add repository cleverly.')
+  .example('add https://github.com/vuejs/vue.git')
+  .example('add git@github.com:vuejs/vue.git')
+  .action(url => {
+    console.log('run add', url)
+  })
+
+prog
+  .command('import <from>')
+  .describe('Import repositories from existing directory.')
+  .example('import ~/Code')
+  .action(from => {
+    console.log('run import', from)
+  })
+
+prog.parse(process.argv)
